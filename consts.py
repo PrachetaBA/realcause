8adeb4b302166e26c618b15abc508c7c4abc3db9
PrachetaBA <pboddavarama@umass.edu>
2024-05-30 19:49:57 +0000

    updated requirements

--- consts.py
+++ consts.py
@@ -1,3 +1,3 @@
-REALCAUSE_DATASETS_FOLDER = 'realcause_datasets'
+REALCAUSE_DATASETS_FOLDER = '/work/pi_jensen_umass_edu/pboddavarama_umass_edu/nfl/realcause/realcause_datasets'
 N_SAMPLE_SEEDS = 100
 N_AGG_SEEDS = 100
