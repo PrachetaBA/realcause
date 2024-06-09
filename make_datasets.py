5e113631294066858ce0acb62ff49de3cffd3082
PrachetaBA <pboddavarama@umass.edu>
2024-06-09 00:29:26 +0000

    work only with lalonde for now

--- make_datasets.py
+++ make_datasets.py
@@ -1,3 +1,8 @@
+"""Script that is used to generate realcause datasets using the models trained 
+by the original authors Neal et al. 
+"""
+
+# Import libraries 
 import pandas as pd
 import numpy as np
 from numpy.testing import assert_approx_equal
@@ -10,21 +15,25 @@ from data.lalonde import load_lalonde
 from data.twins import load_twins
 from consts import REALCAUSE_DATASETS_FOLDER, N_SAMPLE_SEEDS, N_AGG_SEEDS
 
-FOLDER = Path(REALCAUSE_DATASETS_FOLDER)
+FOLDER = Path(REALCAUSE_DATASETS_FOLDER + '/lalonde_psid_setting1')
 FOLDER.mkdir(parents=True, exist_ok=True)
 
 psid_gen_model, args = load_from_folder(dataset='lalonde_psid1')
-cps_gen_model, args = load_from_folder(dataset='lalonde_cps1')
-twins_gen_model, args = load_from_folder(dataset='twins')
+# cps_gen_model, args = load_from_folder(dataset='lalonde_cps1')
+# twins_gen_model, args = load_from_folder(dataset='twins')
 
 psid_w, psid_t, psid_y = load_lalonde(obs_version='psid', data_format='pandas')
-cps_w, cps_t, cps_y = load_lalonde(obs_version='cps', data_format='pandas')
-d_twins = load_twins(data_format='pandas')
-twins_w, twins_t, twins_y = d_twins['w'], d_twins['t'], d_twins['y']
+# cps_w, cps_t, cps_y = load_lalonde(obs_version='cps', data_format='pandas')
+# d_twins = load_twins(data_format='pandas')
+# twins_w, twins_t, twins_y = d_twins['w'], d_twins['t'], d_twins['y']
 
-gen_models = [psid_gen_model, cps_gen_model, twins_gen_model]
-w_dfs = [psid_w, cps_w, twins_w]
-names = ['lalonde_psid', 'lalonde_cps', 'twins']
+gen_models = [psid_gen_model] #, cps_gen_model, twins_gen_model]
+w_dfs = [psid_w] #, cps_w, twins_w]
+names = ['lalonde_psid'] #, 'lalonde_cps', 'twins']
+
+# Compute the naive ATE for PSID
+psid_naive_ate = (psid_y[psid_t == 1].mean() - psid_y[psid_t == 0].mean())
+print('ATE (Naive Biased Estimate) PSID:', psid_naive_ate)
 
 dfs = []
 print('N samples:', N_SAMPLE_SEEDS)
@@ -42,9 +51,12 @@ for gen_model, w_df, name in zip(gen_models, w_dfs, names):
         ates = []
         for seed in range(start_seed, end_seed):
             # print('Seed:', seed)
-            w, t, (y0, y1) = gen_model.sample(w_orig, ret_counterfactuals=True, seed=seed)
+            w, t, (y0, y1) = gen_model.sample(w_orig, ret_counterfactuals=True, seed=seed,
+                                              causal_effect_scale=1.0,  # Set to a value of 1.0 (scaled)
+                                              deg_hetero=0.0,       # Set to a value of 0.0 (no heterogeneity)
+                                              untransform=False) # Set to False so that the y values are left as is
+            
             y = y0 * (1 - t) + y1 * t
-
             # w_errors = np.abs(w_orig - w)
             # assert w_errors.max() < 1e-2
             df = w_df
@@ -53,14 +65,14 @@ for gen_model, w_df, name in zip(gen_models, w_dfs, names):
             df['y0'] = y0
             df['y1'] = y1
             df['ite'] = y1 - y0
-
             ate = (y1 - y0).mean()
             ates.append(ate)
 
         ate_means.append(np.mean(ates))
         df.to_csv(FOLDER / (name + '_sample{}.csv'.format(sample_i)), index=False)
         dfs.append(df)
-
+        
+    print('ATEs: ', ates)
     print('ATE mean mean (min-max) (std): {} ({} - {}) ({})'
           .format(np.mean(ate_means), np.min(ate_means), np.max(ate_means), np.std(ate_means)))
     print('Time elapsed: {} minutes'.format((time.time() - dataset_start) / 60))
