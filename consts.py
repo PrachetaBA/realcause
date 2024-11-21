cedffeaeca6cb65924a1af5f87b3fd5dac920f02
PrachetaBA <pboddavarama@umass.edu>
2024-11-21 05:32:41 +0000

    add location of kunzel dgp

--- consts.py
+++ consts.py
@@ -3,3 +3,4 @@ N_AGG_SEEDS = 1
 ROOT_DIR = '/work/pi_jensen_umass_edu/pboddavarama_umass_edu/nfl/realcause'
 BASE_DATASETS_FOLDER = f'{ROOT_DIR}/base_datasets' # For the base datasets that are being used
 REALCAUSE_DATASETS_FOLDER = f'{ROOT_DIR}/nfl_datasets' # For LaLonde PSID1 datasets
+NFL_BASE_DATASETS_FOLDER = '/work/pi_jensen_umass_edu/pboddavarama_umass_edu/nfl/r-data-gen/datasets' 
